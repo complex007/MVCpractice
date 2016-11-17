9e61b99d094dcbda9ecf9bc24aec82bb511f6c6e
WANG YUQI <E0046644@iss.nus.edu.sg>
2016-11-17 19:20:47 +0800

    Add a Controller

---
+++ MvcMovie/Controllers/HelpWorldController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MvcMovie.Controllers
+{
+    public class HelpWorldController : Controller
+    {
+        // GET: HelpWorld
+        public string Index()
+        {
+            return "This is my <b>default</b> action ...";
+        }
+        //public string Welcome(string name, int  numTimes=1)
+        //{
+        //    return HttpUtility.HtmlEncode("Hello"+name+", NumItmes is : "+numTimes);
+        //}
+        public string Welcome(string name, int ID = 1)
+        {
+            return HttpUtility.HtmlEncode("Hello" + name + ", ID is : " + ID);
+        }
+    }
+}
