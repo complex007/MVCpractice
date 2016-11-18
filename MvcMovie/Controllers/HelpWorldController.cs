45cc8d8ebca170e42006011f01c3d6e0a4b77628
WANG YUQI <E0046644@iss.nus.edu.sg>
2016-11-18 15:57:53 +0800

    Add search

--- MvcMovie/Controllers/HelpWorldController.cs
+++ MvcMovie/Controllers/HelpWorldController.cs
@@ -9,17 +9,28 @@ namespace MvcMovie.Controllers
     public class HelpWorldController : Controller
     {
         // GET: HelpWorld
-        public string Index()
-        {
-            return "This is my <b>default</b> action ...";
-        }
+        //public string Index()
+        //{
+        //    return "This is my <b>default</b> action ...";
+        //}
         //public string Welcome(string name, int  numTimes=1)
         //{
         //    return HttpUtility.HtmlEncode("Hello"+name+", NumItmes is : "+numTimes);
         //}
-        public string Welcome(string name, int ID = 1)
+        //public string Welcome(string name, int ID = 1)
+        //{
+        //    return HttpUtility.HtmlEncode("Hello" + name + ", ID is : " + ID);
+        //}
+        public ActionResult Index()
         {
-            return HttpUtility.HtmlEncode("Hello" + name + ", ID is : " + ID);
+            return View();
         }
+        public ActionResult Welcome(string name, int numTimes=1)
+        {
+            ViewBag.Message = "Hello" + name;
+            ViewBag.NumTimes = numTimes;
+            return View();
+        }
+
     }
 }
